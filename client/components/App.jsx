e0f4e60869442f5f674055942da24c1bfc0f72dc
Sam Sanders <sam-sanders@hotmail.co.uk>
2018-06-18 08:54:00 +1200

    monday morning paws for reflection ... Hi Emily

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Dog from './Dog.jsx'
+import Subtitle from './Subtitle.jsx'
 
 const App = () => (
   <div className='container'>
     <img className='spinner' src='/images/paw.png' />
+    <Subtitle text='Canines using supercanine abilities for social good.'/>
+    <Dog name='Desdemona' breed='Bulldog' superpower='Heat vision' img='/images/bulldog.png' />
+    <Dog name='Delilah' breed='Colli-Cross' superpower='Door opening' />
+    <Dog name='Shame' breed='Sheppard' superpower='Codeing' />
   </div>
 )
 
