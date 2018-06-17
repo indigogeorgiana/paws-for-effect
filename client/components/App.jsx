d06cd01b75b32fef0bf6d9dcb4b969b08658a4d8
Michael Mitchelson <michael.j.mitchelson@gmail.com>
2018-06-18 11:10:05 +1200

    adds flashy but uninspired title

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Dog from './Dog.jsx'
+import Subtitle from './Subtitle.jsx'
 
 const App = () => (
   <div className='container'>
     <img className='spinner' src='/images/paw.png' />
+    <Subtitle text='These are some dogs that idk do things i guess'/>
     <Dog name='Nert' breed='Dalmation' superpower='Just being like super cool' />
     <Dog name='Atomic' breed='Sheepadoodle' superpower='teleport' />
   </div>
