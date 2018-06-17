ac22571d620c46a84cc8350abc00acf6b1e61d7c
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-06-18 11:40:13 +1200

    added map function to loop through dogs

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import Dog from './Dog.jsx'
 
-// const props = {
-//   name: 'Desdemona',
-//   breed: 'Bulldog',
-//   superpower: 'Heat vision'
-// }
+const dogs = [
+  {
+    name: 'Desdemona',
+    breed: 'Bulldog',
+    superpower: 'Heat vision',
+    img: '/images/bulldog.png'
+  },
+  {
+    name: 'Elsie',
+    breed: 'Scottie',
+    superpower: 'Flight',
+    img: '/images/scottie.png'
+  },
+  {
+    name: 'Alice',
+    breed: 'Greyhound',
+    img: '/images/greyhound.png'
+  }
+]
 
 const App = () => (
   <div className='container'>
     <img className='spinner' src='/images/paw.png' />
-    <Dog name='Desdemona' breed='Bulldog' superpower='Heat vision' img='/images/bulldog.png' />
+    {dogs.map(function (dog) {
+      return <Dog key={dog.name} name={dog.name} breed={dog.breed} superpower={dog.superpower} img={dog.img} />
+    })}
+    {/* <Dog name='Desdemona' breed='Bulldog' superpower='Heat vision' img='/images/bulldog.png' />
     <Dog name='Elsie' breed='Poodle' superpower='Flight' img='/images/scottie.png' />
-    <Dog name='Alice' breed='Great Dane' img='/images/greyhound.png' />
+    <Dog name='Alice' breed='Great Dane' img='/images/greyhound.png' /> */}
   </div>
 )
 
