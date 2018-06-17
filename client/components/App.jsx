543301fbf5d189e70c245e59802b9adbae8ee4af
Michael Mitchelson <michael.j.mitchelson@gmail.com>
2018-06-18 10:55:20 +1200

    creates dog jsx element

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Dog from './Dog.jsx'
 
 const App = () => (
   <div className='container'>
     <img className='spinner' src='/images/paw.png' />
+    <Dog name='Nert' breed='Dog' superpower='apathy' />
   </div>
+
+
 )
 
 export default App
